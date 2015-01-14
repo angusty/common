6365227f4424f271bcb2db951017fd10cb79e06a
yboker <8236138@qq.com>
2015-01-14 09:21:58 +0800

    add config

---
+++ php/thinkphp/ucenter-discuz-api/config.discuz.php
@@ -0,0 +1,7 @@
+<?php
+define('UC_KEY', '9b2d056d374f5321cdf30d646d4e2d2f');
+define('UC_API', 'http://bbs.thinkdemo.com/api/uc.php');
+define('UC_CHARSET', 'utf-8');
+define('UC_IP', '');
+define('UC_APPID', '2');
+define('UC_PPP', '20');
---
+++ php/thinkphp/ucenter-discuz-api/config.php
@@ -0,0 +1,15 @@
+<?php
+define('UC_CONNECT', 'mysql');
+define('UC_DBHOST', 'localhost');
+define('UC_DBUSER', 'root');
+define('UC_DBPW', '');
+define('UC_DBNAME', 'ucenter');
+define('UC_DBCHARSET', 'utf8');
+define('UC_DBTABLEPRE', '`ucenter`.uc_');
+define('UC_DBCONNECT', '0');
+define('UC_KEY', 'hhr45666ddhhdzsdfssdf');
+define('UC_API', 'http://localhost.com/ucenter/upload');
+define('UC_CHARSET', 'utf-8');
+define('UC_IP', '');
+define('UC_APPID', '6');
+define('UC_PPP', '20');
