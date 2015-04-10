506e13b991d570b8b26965eb122837d827c64758
yboker <8236138@qq.com>
2015-04-10 13:27:21 +0800

    update socket

--- php/demo/socket/accept/socket_client.php
+++ php/demo/socket/accept/socket_client.php
@@ -1,28 +1,47 @@
 <?php
-function debug($msg)
+/**
+ * Socket Test Client
+ * By James.Huang <shagoo#gmail.com>
+**/
+function debug ($msg)
 {
-    error_log($msg);
+//  echo $msg;
+    error_log($msg, 3, 'socket.log');
 }
 
-if ($argv[1]) {
-    $socket_client = stream_socket_client('tcp://0.0.0.0:2000', $errno, $errstr, 30);
-    if (!$socket_client) {
-        die("{$errstr} ({$errno})");
+if (!empty($argv[1])) {
+
+    $socket_client = stream_socket_client('tcp://localhost:8282', $errno, $errstr, 30);
+
+//  stream_set_blocking($socket_client, 0);
+//  stream_set_timeout($socket_client, 0, 100000);
+
+    if (empty($socket_client)) {
+        die("$errstr ($errno)");
     } else {
         $msg = trim($argv[1]);
-        for($i=0; $i<10; $i++) {
-            $res = fwrite($socket_client, $msg($i));
-            usleep(5000);
-            echo 'W';    //打印写次数
-
+        for ($i = 0; $i < 10; $i++) {
+            $res = fwrite($socket_client, "$msg($i)");
+            usleep(100000);
+            echo 'W'; // 打印写的次数
+//          debug(fread($socket_client, 1024)); // 将产生死锁，因为 fread 在阻塞模式下未读到数据时将等待
         }
-        fwrite($socket_client, "\r\n");     //发送传输 结束
+        fwrite($socket_client, "\r\n"); // 传输结束符
         debug(fread($socket_client, 1024));
         fclose($socket_client);
     }
-}  else {
-    for ($i=0; $i < 10; $i++) {
-        system("php " . __FILE__ . " {$i} : test");
-    }
+}
+else {
 
+//  $phArr = array();
+//  for ($i = 0; $i < 10; $i++) {
+//      $phArr[$i] = popen("php ".__FILE__." '{$i}:test'", 'r');
+//  }
+//  foreach ($phArr as $ph) {
+//      pclose($ph);
+//  }
+
+    for ($i = 0; $i < 10; $i++) {
+        system("php ".__FILE__." '{$i}:test'");
+    }
 }
