0a5e79ef3b3810804d44807113501fe1e6e0799f
yboker <8236138@qq.com>
2015-04-10 09:01:14 +0800

    socket

--- php/demo/websocket/demo.php
+++
@@ -1,8 +0,0 @@
-<?php
-//建立 socket 套接字
-$address = 'localhost';
-$port = '8236';
-$master = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-socket_set_option($master, SOL_SOCKET, SO_REUSEADDR, 1);
-socket_bind($master, $address, $port);
-socket_listen($master);
